f8a56959c33e561d689ed0256380a4d72487e7e5
TS <talha.i.sheikh@gmail.com>
2020-02-24 21:50:34 +0000

    running app from command line

--- venv/hackernews.py
+++ venv/hackernews.py
@@ -1,20 +1,18 @@
 import sys
+import argparse
 from hackerNewsApi import getTopPosts
 
 
-def main():
-    try:
-        userInput = input("Enter number of top posts you want from hacker news: ")
-        val = int(userInput)
-    except ValueError:
-        sys.exit("Please enter a number")
 
-    if (not val <= 100) & (val > 0):
-        sys.exit("Invalid post request. Please request posts between 0 - 100")
-
-    posts = getTopPosts(val)
-    print(posts)
+parser = argparse.ArgumentParser(description='Display top posts from hacker news')
+parser.add_argument('-p','--posts', type=int, help='please provide an integer number for number of posts')
+args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    main()
+
+    if (not args.posts <= 100) & (args.posts > 0):
+        sys.exit("Invalid post request. Please request posts between 0 - 100")
+    posts = getTopPosts(args.posts)
+    for s in posts:
+        print(s)
